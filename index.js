311ae4d4331fa628a858ee822b9d6c59d597be55
moeed038ulazeem <moeedmoeed038@gamil.com>
2024-09-19 04:52:54 -0700

    today i applied style on header section and on services and on hero sections i also apllied js on humburger and some on some below contemt like navbarlist etc

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-const hamburger = document.querySelector(".hamburger")
+const hamburger = document.querySelector(".hamburger");
 
-humburger.addEventListner("click", {} => {
-    humburger.classList.toggle{"active"};
+
+const mobileMenu = document.querySelector(".nav-list ul");
+const menuItem = document.querySelectorAll(".nav-list ul li a")
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobileMenu.classList.toggle("active")
 });
+
+menuItem.forEach((item) => {
+    item.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        mobileMenu.classList.toggle("active");
+    });
+});
+
+document.addEventListener("scroll", () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = "#29323c";
+    } else {
+        header.style.backgroundColor = "transparent"
+    }
+})
